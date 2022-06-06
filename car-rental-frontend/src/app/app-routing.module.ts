a68c030618e04c927ed82ad50654df7b651a4c0f
Kornél Juhász <juhasz.kornel@united-consult.hu>
2022-06-06 15:36:53 +0200

    Create layout

--- car-rental-frontend/src/app/app-routing.module.ts
+++ car-rental-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CarListComponent } from './car-list/car-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'issue-list',
+    component: CarListComponent,
+  },
+  {
+    path: '**',
+    redirectTo: 'issue-list',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- car-rental-frontend/src/app/app.component.ts
+++ car-rental-frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { faMagnifyingGlass, faCar, faUser } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  faMagnifyingGlass = faMagnifyingGlass;
+  faCar = faCar;
+  faUser = faUser;
   title = 'car-rental-frontend';
 }
