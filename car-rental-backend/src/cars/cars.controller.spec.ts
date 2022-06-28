4ffef9062f9d6d1aa5cb38518221e6bcb1594ebe
Kornél Juhász <juhasz.kornel@united-consult.hu>
2022-06-28 14:37:24 +0200

    reepair cars.controller.spec.ts

--- car-rental-backend/src/cars/cars.controller.spec.ts
+++ car-rental-backend/src/cars/cars.controller.spec.ts
@@ -27,7 +27,7 @@ describe('CarsController', () => {
 
   it('should give empty array when no issues have been created', () => {
     carsService.findAll.mockReturnValue([]);
-    expect(controller.findAll({})).resolves.toEqual([]);
+    expect(controller.findAll({}, {})).resolves.toEqual([]);
   });
 
   it('should throw an error when the requested issue is missing', () => {
